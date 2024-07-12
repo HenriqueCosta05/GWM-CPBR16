4574d891ba291b351c2b922422c399f447d49638
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-07-12 13:30:17 -0300

    feat: created user service and its implementations

---
+++ backend-gwm/src/main/java/com/example/backend_gwm/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.backend_gwm.service;
+
+import com.example.backend_gwm.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface UserService {
+    List<User> listAll();
+    Optional<User> listById(String id);
+    User create(User newUser);
+    User update(User newUser);
+    void delete(String id);
+}
---
+++ backend-gwm/src/main/java/com/example/backend_gwm/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.backend_gwm.service.impl;
+
+import com.example.backend_gwm.model.User;
+import com.example.backend_gwm.repository.UserRepository;
+import com.example.backend_gwm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<User> listAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> listById(String id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User create(User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    public User update(User updatedUser) {
+        return userRepository.save(updatedUser);
+    }
+
+    @Override
+    public void delete(String id) {
+        userRepository.deleteById(id);
+    }
+}
