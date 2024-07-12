0ff084519c454a2a5d7d016b356e0ffb52c61010
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-07-12 13:10:10 -0300

    feat: created entity user

---
+++ backend-gwm/src/main/java/com/example/backend_gwm/model/User.java
@@ -0,0 +1,37 @@
+package com.example.backend_gwm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuarios")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phoneNumber;
+
+    @Column(name = "zip")
+    private String zip;
+
+    @Column(name = "password")
+    private String password;
+
+}
