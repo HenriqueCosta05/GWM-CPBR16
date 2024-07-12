5efda5c41c83b01bfffd028cbeb84fb355f06c58
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-07-12 13:10:36 -0300

    feat: created entity user

---
+++ backend-gwm/src/main/java/com/example/backend_gwm/BackendGwmApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_gwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGwmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGwmApplication.class, args);
+	}
+
+}
